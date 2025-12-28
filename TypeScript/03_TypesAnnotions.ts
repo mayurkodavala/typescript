ca289b6d264e07bb193ceeda4727663b93168387
kodavala Mayur <mayurkodavala07@gmail.com>
2025-12-28 06:16:40 +0000

    basic TypesAnnotions

---
+++ TypeScript/03_TypesAnnotions.ts
@@ -0,0 +1,7 @@
+let drink : string = "coffee"
+
+let cups : number | string = Math.random() > 0.5 ? 10 : "5"
+
+let chaiFlavour : string = "masala chai"
+chaiFlavour = "green chai"
+console.log(chaiFlavour);
